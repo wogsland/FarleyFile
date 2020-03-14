5a639efd8aff4d45ad865cabefca057b22cb76bb
Bradley Wogsland <bradley@wogsland.org>
2020-03-14 12:51:09 +0100

    WIP

--- FarleyFile/Files.py
+++ FarleyFile/Files.py
@@ -2,12 +2,19 @@ import os
 
 
 class Files():
+
+    def __init__(self):
+        'Initialization'
+        self.directory = 'files'
+
     def getFileNames(self):
         'gets a list of filenames'
-        fileNames = os.listdir('files')
+        fileNames = os.listdir(self.directory)
         fileNames.remove('.gitignore')
         return fileNames
 
     def getNextFileName(self):
         'gets the next expected fileName'
-        print('wait')
+        lastFileName = self.getFileNames()
+        #return lastFileName
+        return 12
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,12 +1,19 @@
 from unittest import TestCase
-from FarleyFile import Files
+from FarleyFile.Files import Files
 
 
 class TestFiles(TestCase):
 
+    def test_init(self):
+        files = Files()
+
     def test_getFileNames(self):
-        print('test')
-        files = new Files()
+        files = Files()
         fileNames = files.getFileNames()
         self.assertEqual(1, len(fileNames))
         self.assertEqual('1.json', fileNames[0])
+
+    def test_getNextFileName(self):
+        files = Files()
+        nextFileName = files.getNextFileName()
+        self.assertEqual('2.json', nextFileName)
